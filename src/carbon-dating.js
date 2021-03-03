229453e453cafe454ed6267e7c4bba8dd94803c7
@KSGitUser <rusakk@mail.ru>
2021-03-03 22:14:29 +0300

    fix: fix dateSample wrong type detection

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -5,10 +5,10 @@ const HALF_LIFE_PERIOD = 5730;
 const K = 0.693;
 
 module.exports = function dateSample(sampleActivity) {
-  const intSampleActivity = parseInt(sampleActivity);
+  const intSampleActivity = +sampleActivity;
   if (!intSampleActivity ||
+    typeof intSampleActivity !== 'number' ||
     typeof sampleActivity !== 'string' ||
-    intSampleActivity !== 'number' ||
     intSampleActivity <= 0 || intSampleActivity >= 15) {
     return false;
   }
