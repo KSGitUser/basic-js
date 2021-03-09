531fcd9e97e24e2029cc3286d8e412c2c40a669b
@KSGitUser <rusakk@mail.ru>
2021-03-09 18:02:24 +0300

    feat: add repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options = {}) {
+  defaulOptions = { separator: '+', additionSeparator: '|', repeatTimes: 1 };
+  options = { ...defaulOptions, ...options };
+
+  str = String(str);
+
+  if (!str) {
+    return '';
+  }
+
+  let fullString = '';
+
+  if (options) {
+    if (Reflect.has(options, 'repeatTimes')) {
+      let separator = '';
+      if (Reflect.has(options, 'separator')) {
+        separator = options.separator;
+      }
+      for (let i = 1; i <= options.repeatTimes; i++) {
+        let additionStr = '';
+        if (Reflect.has(options, 'addition')) {
+          additionStr +=
+            repeater(options.addition, {
+              repeatTimes: options.additionRepeatTimes || 1,
+              separator: options.additionSeparator || '|'
+            });
+        }
+        fullString += str + additionStr + (i !== options.repeatTimes ? separator : '');
+
+      }
+    }
+  }
+  return fullString;
 };
-  
